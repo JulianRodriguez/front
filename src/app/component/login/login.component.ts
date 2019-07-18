b7c7420108348fda7cec205284a8c2b85a1cb633
JulianRodriguez <jrcarave91@gmail.com>
2019-07-18 09:06:54 +0200

    actualizacion

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -16,6 +16,7 @@ export class LoginComponent implements OnInit {
   constructor(private loginService: LoginService, private router: Router, private userService: UserService) { }
 
   ngOnInit() {
+
   }
 
   logIn(username: string, password: string, event: Event) {
